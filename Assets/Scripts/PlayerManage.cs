17ac389c124373badbe65bafce7c68580877f1b7
Soham Das <79685489+Mark-42-max@users.noreply.github.com>
2022-04-11 22:02:43 +0530

    Behaviour Script Added

---
+++ Assets/Scripts/PlayerManage.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManage : MonoBehaviour
+{
+    [SerializeField]private Rigidbody2D rb;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+    public void Jump() {
+        Vector3 move = new Vector3(0f, 30f, 0f);
+        rb.AddForce(move);
+    }
+}
